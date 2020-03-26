ac25d0b1f5ef68ac35f3696dd671694e4290d430
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-26 01:00:26 +0100

    Binding Request params instead of HttpServletRequest

--- SpringMVC/WebContent/WEB-INF/view/initialForm.jsp
+++ SpringMVC/WebContent/WEB-INF/view/initialForm.jsp
@@ -4,9 +4,9 @@
 <title>Hello World Input Form</title>
 </head>
 <body>
-	<form action="processFormVersionTwo" method="GET">
-		<input type="text" name="studentName"
-			placeholder="What is your name?" /> <input type="submit" />
+	<form action="processFormVersionThree" method="GET">
+		<input type="text" name="studentName" placeholder="What is your name?" />
+		<input type="submit" />
 	</form>
 </body>
 </html>
--- SpringMVC/src/SpringMVC/HelloWorldController.java
+++ SpringMVC/src/SpringMVC/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -14,12 +15,10 @@ public class HelloWorldController {
 		return "initialForm";
 	}
 
-	@RequestMapping("/processForm")    
+	@RequestMapping("/processForm")
 	public String showSuccess() {
 		return "helloWorld";
 	}
-	
-	
 
 	// new controller method to read form data
 	@RequestMapping("/processFormVersionTwo")
@@ -36,7 +35,23 @@ public class HelloWorldController {
 
 		// add message to the model
 		model.addAttribute("message", result);
-		
+
 		return "helloWorld";
 	}
+
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+
+		// create the message
+		String result = "Hey My Friend V3 " + theName;
+
+		// add message to the model
+		model.addAttribute("message", result);
+
+		return "helloWorld";
+	}
+
 }
